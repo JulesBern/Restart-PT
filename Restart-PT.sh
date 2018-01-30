f6e5d9e1f256f7601be5f988fa847dfc6a8c178a
J.B <JulesBern@users.noreply.github.com>
2018-01-30 13:15:24 -0500

    Update Restart-PT.sh

--- Restart-PT.sh
+++ Restart-PT.sh
@@ -3,7 +3,7 @@
 # Restart-PT.sh
 # You can schedule a cron job to run at whatever time intervals to restart the bot (the example below runs every 6 hours)
 
-# * */6 * * * /home/ProfitTrailer/Restart-PT.sh >/dev/null 2>&1
+# 0 */6 * * * /home/ProfitTrailer/Restart-PT.sh >/dev/null 2>&1
 
 # Change directory to the location of ProfitTrailer
 cd /home/ProfitTrailer
